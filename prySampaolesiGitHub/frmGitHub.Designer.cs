67d4ff82a68c1312b82e023d6e8710785cf5bc35
Ivo Sampaolesi <ivodribe@gmail>
2025-08-27 12:43:54 -0300

    Agregue Etiqueta

--- prySampaolesiGitHub/frmGitHub.Designer.cs
+++ prySampaolesiGitHub/frmGitHub.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            label1 = new Label();
             SuspendLayout();
             // 
             // button1
@@ -40,19 +41,32 @@
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(218, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            label1.Click += this.label1_Click;
+            // 
             // frmGitHub
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(504, 283);
+            Controls.Add(label1);
             Controls.Add(button1);
             Name = "frmGitHub";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Label label1;
     }
 }
